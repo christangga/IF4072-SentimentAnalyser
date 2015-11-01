bf8eff7ff5107b5e1b266c39ab9a319528218b46
Christ Angga Saputra <christanggasaputra@gmail.com>
2015-11-01 21:00:39 +0700

    update java

--- src/if4072/sentimentanalysis/IF4072SentimentAnalysis.java
+++ src/if4072/sentimentanalysis/IF4072SentimentAnalysis.java
@@ -8,7 +8,12 @@ package if4072.sentimentanalysis;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -27,34 +32,28 @@ public class IF4072SentimentAnalysis {
      * @param args the command line arguments
      * @throws twitter4j.TwitterException
      * @throws java.io.FileNotFoundException
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) throws TwitterException, FileNotFoundException {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        if (args[0].equals("-p")) {
-            cb.setHttpProxyHost("cache.itb.ac.id");
-            cb.setHttpProxyPort(8080);
-            cb.setHttpProxyUser(args[1]);
-            cb.setHttpProxyPassword(args[2]);
-        }
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("T9NUdDX6v1h9ePI61RKi4g")
-                .setOAuthConsumerSecret(
-                        "xi4EIC9so6e6n8oOM3ggx2zbayj3JWhSpEj165bEJU")
-                .setOAuthAccessToken(
-                        "178369831-97z3fDZvMH0e8Ne0ot7WzFXayAPSU2eOx9VzLr4y")
-                .setOAuthAccessTokenSecret(
-                        "KwTHAIi2bWAVWOPtNM4NoYaiAwLvrkGWqWHv6jGBKffF5");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
+    public static void main(String[] args) throws TwitterException, FileNotFoundException, InterruptedException {
 
+        PrintStream out = new PrintStream(new FileOutputStream("data/" + "gojek" + ".csv"));
+        System.setOut(out);
+
+        String proxyUsername = "";
+        String proxyPassword = "";
         String queryString = "";
+
         if (args[0].equals("-p")) {
+            proxyUsername = args[1];
+            proxyPassword = args[2];
             for (int i = 3; i < args.length; ++i) {
                 queryString += args[i] + " ";
             }
         } else {
             for (int i = 0; i < args.length; ++i) {
                 if (args[i].equals("-p")) {
+                    proxyUsername = args[i + 1];
+                    proxyPassword = args[i + 2];
                     break;
                 } else {
                     queryString += args[i] + " ";
@@ -63,26 +62,64 @@ public class IF4072SentimentAnalysis {
         }
         queryString = queryString.trim();
 
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        cb.setOAuthConsumerKey("T9NUdDX6v1h9ePI61RKi4g");
+        cb.setOAuthConsumerSecret("xi4EIC9so6e6n8oOM3ggx2zbayj3JWhSpEj165bEJU");
+        cb.setOAuthAccessToken("178369831-97z3fDZvMH0e8Ne0ot7WzFXayAPSU2eOx9VzLr4y");
+        cb.setOAuthAccessTokenSecret("KwTHAIi2bWAVWOPtNM4NoYaiAwLvrkGWqWHv6jGBKffF5");
+
+        if (args[0].equals("-p")) {
+            cb.setHttpProxyHost("cache.itb.ac.id");
+            cb.setHttpProxyPort(8080);
+            cb.setHttpProxyUser(proxyUsername);
+            cb.setHttpProxyPassword(proxyPassword);
+        }
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
         Query query = new Query(queryString + " +exclude:retweets");
         query.setCount(100);
-        QueryResult result = twitter.search(query);
-        
-        try (PrintStream out = new PrintStream(new FileOutputStream("data/output.csv"))) {
-            System.setOut(out);
 
-            System.out.println("\"content\",\"original_id\",\"from\",\"date_created\",\"sentiment\",\"original_sentiment\"");
-            for (int i = 0; i < 200; ++i) {
-                result.getTweets().stream().forEach((status) -> {
+        HashSet<String> tweetSet = new HashSet<>();
+        long lastID = Long.MAX_VALUE;
+        int totalRequest = 0;
+        
+        System.out.println("\"content\",\"original_id\",\"from\",\"date_created\",\"sentiment\",\"original_sentiment\"");
+        while (tweetSet.size() < 10000) {
+            QueryResult result = twitter.search(query);
+            totalRequest++;
+            
+            for (Status status : result.getTweets()) {
+                List<String> tweetStatus = new LinkedList<>(Arrays.asList(status.getText().replace("\"", "'").replace(".", "").replace(":", "").replace("-", "").replace(",", "").replace("\n", " ").replace("\n\r", " ").replace("\r", " ").split(" ")));
+                for (int j = tweetStatus.size() - 1; j >= 0; --j) {
+                    if (tweetStatus.get(j).contains("http") || tweetStatus.get(j).startsWith("#")) {
+                        // System.out.println("tmp" + String.join(" ", tweetStatus));
+                        tweetStatus.remove(j);
+                    }
+                }
+
+                // System.out.println(tweetStatus.toString());
+                if (tweetSet.add(String.join(" ", tweetStatus))) {
+                    // tweetSet.add(String.join(" ", tweetStatus));
+                    // System.out.println(String.join(" ", tweetStatus));
                     System.out.println("\"" + status.getText() + "\",\"" + status.getId() + "\",\"" + status.getUser().getScreenName() + "\",\"" + getDateString(status.getCreatedAt()) + "\",\"\",\"\"");
-                });
-                result.nextQuery();
+                }
+
+                lastID = status.getId();
+            }
+            query.setMaxId(lastID - 1);
+            
+            if (totalRequest >= 400) {
+                TimeUnit.MINUTES.sleep(16);
             }
         }
     }
 
     public static String getDateString(Date d) {
         String dateString = "";
-        
+
         dateString += String.format("%02d", d.getYear() + 1900) + "-";
         dateString += String.format("%02d", d.getMonth() + 1) + "-";
         dateString += String.format("%02d", d.getDate()) + " ";
